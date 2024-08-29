389fa48f640ffafe7c1d379b3fec4305e8fecdfb
taqiynurr <faqihuddintaqiy35@gmail.com>
2024-08-29 14:59:53 +0800

    Add files via upload

---
+++ ukk_galerifoto.sql
@@ -0,0 +1,226 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 22, 2024 at 08:21 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ukk_galerifoto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `album`
+--
+
+CREATE TABLE `album` (
+  `albumid` int(11) NOT NULL,
+  `namaalbum` varchar(225) NOT NULL,
+  `deskripsi` text NOT NULL,
+  `tanggalbuat` date NOT NULL,
+  `userid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `album`
+--
+
+INSERT INTO `album` (`albumid`, `namaalbum`, `deskripsi`, `tanggalbuat`, `userid`) VALUES
+(17, 'keluarga', 'indahnya hidup', '2024-08-21', 24);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `foto`
+--
+
+CREATE TABLE `foto` (
+  `fotoid` int(11) NOT NULL,
+  `judulfoto` varchar(225) NOT NULL,
+  `deskripsifoto` text NOT NULL,
+  `tanggalunggah` date NOT NULL,
+  `lokasifile` varchar(225) NOT NULL,
+  `albumid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `foto`
+--
+
+INSERT INTO `foto` (`fotoid`, `judulfoto`, `deskripsifoto`, `tanggalunggah`, `lokasifile`, `albumid`, `userid`) VALUES
+(18, 'keluarga', 'indahnya', '2024-08-21', '1645740819-rerom love hendra.jpg', 17, 24);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `komentarfoto`
+--
+
+CREATE TABLE `komentarfoto` (
+  `komentarid` int(11) NOT NULL,
+  `fotoid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `isikomentar` text NOT NULL,
+  `tanggalkomentar` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likefoto`
+--
+
+CREATE TABLE `likefoto` (
+  `likeid` int(11) NOT NULL,
+  `fotoid` int(11) NOT NULL,
+  `userid` int(11) NOT NULL,
+  `tanggallike` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `userid` int(11) NOT NULL,
+  `username` varchar(225) NOT NULL,
+  `password` varchar(225) NOT NULL,
+  `email` varchar(225) NOT NULL,
+  `namalengkap` varchar(225) NOT NULL,
+  `alamat` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`userid`, `username`, `password`, `email`, `namalengkap`, `alamat`) VALUES
+(24, 'admin', '21232f297a57a5a743894a0e4a801fc3', 'admin@gmail.com', 'admmin', 'perdamaian');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `album`
+--
+ALTER TABLE `album`
+  ADD PRIMARY KEY (`albumid`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexes for table `foto`
+--
+ALTER TABLE `foto`
+  ADD PRIMARY KEY (`fotoid`),
+  ADD KEY `albumid` (`albumid`),
+  ADD KEY `user id` (`userid`);
+
+--
+-- Indexes for table `komentarfoto`
+--
+ALTER TABLE `komentarfoto`
+  ADD PRIMARY KEY (`komentarid`),
+  ADD KEY `fotoid` (`fotoid`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexes for table `likefoto`
+--
+ALTER TABLE `likefoto`
+  ADD PRIMARY KEY (`likeid`),
+  ADD KEY `fotoid` (`fotoid`),
+  ADD KEY `userid` (`userid`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`userid`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `album`
+--
+ALTER TABLE `album`
+  MODIFY `albumid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `foto`
+--
+ALTER TABLE `foto`
+  MODIFY `fotoid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `komentarfoto`
+--
+ALTER TABLE `komentarfoto`
+  MODIFY `komentarid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `likefoto`
+--
+ALTER TABLE `likefoto`
+  MODIFY `likeid` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `album`
+--
+ALTER TABLE `album`
+  ADD CONSTRAINT `album_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `foto`
+--
+ALTER TABLE `foto`
+  ADD CONSTRAINT `foto_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `foto_ibfk_2` FOREIGN KEY (`albumid`) REFERENCES `album` (`albumid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `komentarfoto`
+--
+ALTER TABLE `komentarfoto`
+  ADD CONSTRAINT `komentarfoto_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `komentarfoto_ibfk_2` FOREIGN KEY (`fotoid`) REFERENCES `foto` (`fotoid`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `likefoto`
+--
+ALTER TABLE `likefoto`
+  ADD CONSTRAINT `likefoto_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `likefoto_ibfk_2` FOREIGN KEY (`fotoid`) REFERENCES `foto` (`fotoid`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
